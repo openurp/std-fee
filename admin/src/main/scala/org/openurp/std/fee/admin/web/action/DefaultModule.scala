d321f6e218267c1340ebac847d51b6167f19e46a
段体华 <duantihua@gmail.com>
2020-09-27 17:09:36 +0800

    move module from edu to std

---
+++ admin/src/main/scala/org/openurp/std/fee/admin/web/action/DefaultModule.scala
@@ -0,0 +1,40 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright © 2014, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openurp.std.fee.admin.web.action
+
+import org.beangle.cdi.bind.BindModule
+import org.openurp.std.fee.admin.utils.StudentUtils
+import org.openurp.std.fee.pay.impl.SufePayServiceImpl
+
+class DefaultModule extends BindModule {
+  override protected def binding(): Unit = {
+    bind(classOf[BillAction], classOf[BillSearchAction])
+    bind(classOf[OrderAction])
+
+    bind(classOf[OnlinePaySettingAction])
+    bind(classOf[FeeTypeConfigAction])
+
+    bind(classOf[BillStatAction])
+
+    bind(classOf[TuitionConfigAction])
+    bind(classOf[SufePayServiceImpl])
+
+    bind( classOf[StudentUtils] )
+  }
+}
